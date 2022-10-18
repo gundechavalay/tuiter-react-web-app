a1e41fc02bc06cb7f1543e025457134f2db3dd70
Valay Gundecha <98115238+valaygundecha@users.noreply.github.com>
2022-10-18 00:30:37 -0400

    a5 JavaScript fall22

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, {} from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to web dev.
@@ -17,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
